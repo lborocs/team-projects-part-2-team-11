4139738b42125d958c32f91828f1a1b3563a5ac9
EmmanuelAdio <emmanueladio065@gmail.com>
2024-02-12 14:25:11 +0000

    Testing VM

--- PKMS(Productivity)/ProdFunctions.js
+++ PKMS(Productivity)/ProdFunctions.js
@@ -1,2 +1,190 @@
-//functions taht will collect the specified user data
-//make them usable for the the website
+//functions that will collect the specified user data
+//make them usable for the the websites
+var mains = document.getElementsByClassName("main");
+User_ = 'frederick.umah@makeitall.org.uk';
+updateAssignedTo(null);
+permission = 'false';
+
+
+////code for current tasks and Archived Tasks////
+document.getElementById("CurrentTasksB").onclick = function() {
+  permission = 'false';
+  updateAssignedTo(null);
+
+  Array.from(mains).forEach(element => {
+      if (element.id != "CurTasks"){
+        element.style.display = "none";
+      }
+    });
+  document.getElementById("CurTasks").style.display = "block";
+  //  $('#listOfTasks').load(document.URL +  ' #listOfTasks');
+}
+
+document.getElementById("ArchivedTasksB").onclick = function() {
+  permission = 'false';
+  Array.from(mains).forEach(element => {
+      if (element.id != "ArchTasks"){
+        element.style.display = "none";
+      }
+    });
+  document.getElementById("ArchTasks").style.display = "block";
+}
+
+////code for each project////
+function getProjectTasks(id){
+  var projID = id;
+  $('#Project_ID').val(projID);
+  permission = 'false';
+
+  Array.from(mains).forEach(element => {
+    if (element.id != "ProjectTasks"){
+      element.style.display = "none";
+    }
+  });
+  document.getElementById("ProjectTasks").style.display = "block";
+
+  $.ajax({
+    url:"ProjectsTasks.php",
+    type: "POST",
+
+    data:{
+      Project_ID : projID,
+      User : User_
+    },
+    success: function(data){
+      $('#ProjectTasks .tasks').html(data)
+    } ,
+    error: function(e){
+      console.log(e.message)
+    }
+  });
+}
+
+function getProjectTasks_Leader(id){
+  var projID = id;
+  permission = 'true';
+
+  $('#Project_ID').val(projID);
+  updateAssignedTo(projID)
+
+  Array.from(mains).forEach(element => {
+    if (element.id != "LeadProjectTasks"){
+      element.style.display = "none";
+    }
+  });
+  document.getElementById("LeadProjectTasks").style.display = "block";
+
+  $.ajax({
+    url:"ProjectsTasks_Leader.php",
+    type: "POST",
+
+    data:{Project_ID : projID},
+    success: function(data){
+      $('#LeadProjectTasks .tasks').html(data)
+    } ,
+    error: function(e){
+      console.log(e.message)
+    }
+  });
+}
+
+//// code for the create tasks model ////
+$(".createTasksBtn").click(openModal);
+function openModal() {
+  $('#createTaskModal').removeClass("hidden");
+  $('.overlay').removeClass("hidden");
+
+  $("#createTaskModal").css("display", "block");
+  $("#Overlay").css("display", "block");
+};
+
+function openTaskModal() {
+  $('#taskModal').removeClass("hidden");
+  $('.overlay').removeClass("hidden");
+
+  $("#taskModal").css("display", "block");
+  $("#Overlay").css("display", "block");
+};
+
+function closeModal() {
+  $('.modal').addClass("hidden");
+  $('.overlay').addClass("hidden");
+
+  $(".modal").css("display", "none");
+  $(".overlay").css("display", "none");
+};
+
+$(document).on("keydown", function (e) {
+    if (e.key === "Escape" && !$('#createTaskModal').hasClass("hidden")) {
+        closeModal();
+    }
+});
+
+//// code to create task////
+$('#SubmitB').click(function(){
+  $.ajax({
+    url:"createTask.php",
+    type: "POST",
+
+    data:{
+      taskTitle : $('#taskTitle').val(),
+      taskDescription : $('#taskDescription').val(),
+      taskDeadline : $('#taskDeadline').val(),
+      assignedTo : $('#assignedTo').val(),
+      Project_ID : $('#Project_ID').val()
+    },
+    success: function(data){
+      alert("Task Created - Refresh page to access the new task");
+      closeModal();
+    } ,
+    complete : function(){
+      $('#listOfTasks').load(document.URL +  ' #listOfTasks');
+      $('#createTaskForm').trigger("reset");
+      //updateAssignedTo(null)
+    }
+  });
+  
+});
+
+function updateAssignedTo(ID){
+  $('#Project_ID').val(ID);
+  $.ajax({
+    url:"UserOptions.php",
+    type: "POST",
+
+    data:{
+      Project_ID : $('#Project_ID').val(),
+      User : User_
+    },
+    success: function(data){
+      $('#assignedTo').html(data)
+    },
+    error: function(e){
+      console.log('Error')
+    }
+  });
+}
+
+////When a task is created////
+function getTaskDetails(id){
+  var taskID = id;
+  openTaskModal();
+  $('#Task_ID').val(id);
+  User_ = 'frederick.umah@makeitall.org.uk';
+
+  $.ajax({
+    url:"GetTaskDetails.php",
+    type: "POST",
+    data:{
+      Task_ID : taskID,
+      User : User_,
+      Permission : permission
+    },
+    success: function(data){
+      $('#taskInfo').html(data)
+    } ,
+    error: function(e){
+      console.log(e.message)
+    }
+  });
+}
--- PKMS(Productivity)/ProductivityPage.php
+++ PKMS(Productivity)/ProductivityPage.php
@@ -1,57 +1,42 @@
 <!DOCTYPE html>
 <html style="background-color:FFFFFF">
 <head>
-    <link rel="stylesheet" href="styles2.css">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <link rel="stylesheet" href="ManagerDraft.css">
     <title> PKMS DashBoard</title>
 </head>
 <body>
+
     <?php
         //set up the connection to the data base
         $username = "team011";
         $password = "JAEWyfUXpzqank7scpWm";
         $servername = "localhost";
         $dbname = "team011";
-    
+
         // Create connection
         $conn = mysqli_connect($servername, $username, $password, $dbname);
-    
+
         // Check connection
         if (!$conn) {
             die("Connection failed: " . mysqli_connect_error());
         }
 
-        $User = 'frederickumah@makeitall.org.uk'
-        
-        //get all tasks
-        $tasksQuery = 'SELECT * 
-        FROM `Tasks` 
-        WHERE user_email = "frederickumah@makeitall.org.uk"'
-
-        allTasks = mysqli_query($conn, $tasksQuery)
-
-        $tasksArray = array();
-        //echo "<datalist id = 'possibleCountries'>";
-        if (mysqli_num_rows($allTasks) > 0){
-            //output data of each row
-            while ($row = mysqli_fetch_array($allTasks)){
-            echo $row;
-            //$possibleCountries = $row[0];
-            }
+        if ($_SERVER['REQUEST_METHOD'] == "POST"){
+            $User = $_POST['$User'];
+        } else {
+            $User = "";
         }
-        //echo "</datalist>";
-
-        // INSERT INTO `Tasks` (`task_id`, `Title`, `Description`, `Deadline`, `user_email`) 
-        // VALUES ('103', 'Testing Task', '', '2023-12-31', 'frederickumah@makeitall.org.uk');
-
     ?>
 
-    <!-- Just a little something -->
     <!--PRODUCTIVITY PAGES-->
     <div id="productivitypage">
         <!-- ------------------------------------------Side bar--------------------------------------------------------- -->
         <!-- code for the side bar -->
         <div class="sidenav">
-        <!-- my tasks sections section 1 -->
+            <!-- my tasks sections section 1 -->
             <div>
             <h3>My Tasks</h3>
             <a id="CurrentTasksB" href="#MTasks">My Tasks</a>
@@ -59,10 +44,67 @@
             </div>
         
             <!-- my projects sections section 2 -->
-            <div style="height: 68%;">
+            <div style="height: 30%;">
             <h3>My Projects</h3>
-            <a id="Project1B" href="#Proj1">Project 1</a>
-            <a id="Project2B" href="#Proj2">Project 2</a>
+            <!-- Collect all the project that the user is in and display the project number in their side bar -->
+    
+            <?php
+                try{
+                    $User = 'frederick.umah@makeitall.org.uk';
+                    
+                    //get all tasks
+                    $projQuery = "SELECT DISTINCT `Tasks`.`project_id`,  `Project`.`name`
+                    FROM `Tasks` JOIN `Project` ON `Tasks`.`project_id` = `Project`.`project_id`
+                    WHERE user_email = '$User' and `Tasks`.`project_id` != '0'
+                    ORDER BY `Tasks`.`project_id` ASC;";
+                    $allProjs = mysqli_query($conn, $projQuery);
+
+        
+                    if (mysqli_num_rows($allProjs) > 0){
+                        //output data of each row
+                        while ($row = mysqli_fetch_array($allProjs)){
+                            $ProjectID = $row[0];
+                            echo "<a id='$ProjectID' class = 'projectB' href='#Project:$ProjectID' onclick = 'getProjectTasks(this.id)'> ".$row[1]."</a>";
+                        }
+                    }
+        
+                }catch(Exception $e){
+                    //catch whatever exception is thrown first and display it's message.
+                    echo "Error: ".$e->getMessage();
+                }
+            ?>
+            </div>
+
+            <!-- my projects sections section 2 -->
+            <div style="height: 30%;">
+            <h3>Projects I Lead</h3>
+            <!-- Collect all the project that the user is in and display the project number in their side bar -->
+    
+            <?php
+                try{
+                    $User = 'frederick.umah@makeitall.org.uk';
+                    
+                    //get all tasks
+                    $projQuery = "SELECT project_id, `name`
+                    FROM `Project` 
+                    WHERE team_leader = '$User'
+                    ORDER BY project_id ASC;";
+                    $allProjs = mysqli_query($conn, $projQuery);
+
+        
+                    if (mysqli_num_rows($allProjs) > 0){
+                        //output data of each row
+                        while ($row = mysqli_fetch_array($allProjs)){
+                            $ProjectID = $row[0];
+                            echo "<a id='$ProjectID' class = 'projectB' href='#Project:$ProjectID' onclick = 'getProjectTasks_Leader(this.id)'> ".$row[1]."</a>";
+                        }
+                    }
+        
+                }catch(Exception $e){
+                    //catch whatever exception is thrown first and display it's message.
+                    echo "Error: ".$e->getMessage();
+                }
+            ?>
             </div>
         </div>
 
@@ -71,110 +113,132 @@
         <div id = "CurTasks", class = "main">
             <!--This will be the place where current task will be displayed.-->
             <h3>Current Tasks</h3>
-            <div>
-                <a href="#Task1">Task 1</a>
-                <a href="#Task2">Task 2</a>
+            <div id = "listOfTasks">
+                <?php
+                    try{
+                        $User = 'frederick.umah@makeitall.org.uk';
+                        
+                        //get all tasks
+                        $tasksQuery = "SELECT * FROM `Tasks` 
+                        WHERE user_email = '$User' AND status = 'INCOMPLETE';";
+                        $allTasks = mysqli_query($conn, $tasksQuery);
+
+            
+                        if (mysqli_num_rows($allTasks) > 0){
+                            //output data of each row
+                            while ($row = mysqli_fetch_array($allTasks)){
+                                echo "<a href = '#Task:$row[0]' onclick = 'getTaskDetails($row[0])'> task : [".$row[0]."] -> ".$row[1]."</a>";
+                            }
+                        }
+            
+                    }catch(Exception $e){
+                        //catch whatever exception is thrown first and display it's message.
+                        echo "Error: ".$e->getMessage();
+                    }
+                ?>
             </div>
+            <button class ="createTasksBtn" class="submit-button">Create Task</button>
         </div>
     
         <div id = "ArchTasks" class = "main" style="display: none;">
             <!--This will be the place where Archived task will be displayed.-->
             <h3>Archived Tasks</h3>
             <div class = "tasks">
-                <a href="#Task1">Task 1 - Complete</a>
-                <a href="#Task2">Task 2 - Complete</a>
+                <!-- <a href="#Task1">Task 1 - Complete</a>
+                <a href="#Task2">Task 2 - Complete</a> -->
+                <?php
+                    try{
+                        $User = 'frederick.umah@makeitall.org.uk';
+                        
+                        //get all tasks
+                        $tasksQuery = "SELECT * FROM `Tasks` 
+                        WHERE user_email = '$User' AND status = 'COMPLETE';";
+                        $allTasks = mysqli_query($conn, $tasksQuery);
+            
+                        //$tasksArray = array();
+                        //echo "<datalist id = 'possibleCountries'>";
+            
+                        if (mysqli_num_rows($allTasks) > 0){
+                            //output data of each row
+                            while ($row = mysqli_fetch_array($allTasks)){
+                                echo "<a href = '#Task:$row[0]' onclick = 'getTaskDetails($row[0])'> task : [".$row[0]."] -> ".$row[1]."</a>";
+                                //$possibleCountries = $row[0];
+                            }
+                        }
+            
+                    }catch(Exception $e){
+                        //catch whatever exception is thrown first and display it's message.
+                        echo "Error: ".$e->getMessage();
+                    }
+                ?>
             </div>
         </div>
     
-        <div id = "Project1Tasks" class = "main" style="display: none;">
-            <!--This will be the page where the project1 tasks will be displayed.-->
-            <h3>Project 1 Tasks</h3>
-    
-            <div class = "tasks_Table">
-                <table>
-                    <tr><th>Unassigned</th> <th>Assigned</th> <th>In-Progress</th> <th>Team members</th></tr>
-                    <tr><td><a href="#Task2">Task 2</a></td><td><a href="#Task1">Task 1</a></td><td></td><td><a href="#Task4">Task 4</a></td></tr>
-                    <tr><td><a href="#Task3">Task 3</a></td><td></td><td></td><td><a href="#Task5">Task 5</a></td></tr>
-                    <tr><td><a href="#Task6">Task 6</a></td><td></td><td></td><td></td></tr>
-                </table>
-            </div>
-    
-            <button id="createTasksBtn">Create Task</button>
-        </div>
-    
-        <div id = "Project2Tasks" class = "main" style="display: none;">
-            <!--This will be the main part of the page where things will be displyed, such as tasks, and all the buttons and etc.-->
-            <h3>Project 2 Tasks</h3>
-            <div class = "tasks">
-                <a href="#Task1">Task 1</a>
-                <a href="#Task2">Task 2</a>
+        <div id = "ProjectTasks" class = "main" style="display: none;">
+            <!--This will be the page where the project's tasks will be displayed.-->
+            <h3>Project  Tasks</h3>
+            <div id = "ProjTasks" class = "tasks">
+            <!-- This is where the project's task list will be displayed -->
             </div>
         </div>
-  
-      
+
+        <div id = "LeadProjectTasks" class = "main" style="display: none;">
+            <!--This will be the page where the project's tasks will be displayed.-->
+            <h3>Project  Tasks</h3>
+            <div id = "leadProjTasks" class = "tasks">
+            <!-- This is where the project's task list will be displayed -->
+            </div>
+            <button class ="createTasksBtn" class="submit-button">Create Task</button>
+        </div>
+    
 
     </div>
     
      <!-- ------------------------------------------all the Modals(pop-ups)--------------------------------------------------------- -->
 
-     <!-- <div id="CreateTaskModal" class="modal">
-        
-        <div class="form-container">
-          
-          <form class="form">
-            <span class="close">&times;</span> 
-              <h1>Create a Task</h1>
-              <label for="taskName">Task Name:</label>
-              <input type="text" id="taskName" name="taskName" required><br><br>
-  
-              <label for="taskDescription">Task Description:</label>
-              <input type="text" id="taskDescription" name="taskDescription"><br><br>
-  
-              <br>
-              <label for="taskAssignee">Choose Team Member:</label>
-              <select id="taskAssignee" name="taskAssignee">
-                  <option value="Member 1">Member 1</option>
-                  <option value="Member 2">Member 2</option>
-                  <option value="Member 3">Member 3</option>
-              </select><br><br>
-              <div class="button-container">
-                  <input type="submit" value="Create Task">
-              </div>
-          </form>
+    <!-- modal for create a Task -->
+    <div id = "Overlay" class="overlay hidden"></div> 
+    <section id = "createTaskModal" class="modal hidden"> 
+        <div class="flex">
+            <button class="btn-close" onclick = "closeModal()";>⨉</button>
         </div>
-      </div>
-  
-      <div id="TaskDetails" class="modal">
-       
-        <div class="modal-content">
-          <span class="close">&times;</span> 
-          <h1 id = "TaskTitle"></h1>
-  
-          <p>Team Leader : *Username*</p>
-          <p>Date Assigned : *Date*</p>
-  
-          <div style=" padding: 20px; border: #111; border-width: 1px; border-radius: 10px;">
-            <p>Description</p>
-          </div>
-  
-          <div style="display: flex">
-            <label for="Status">Status:</label>
-            <select id="Status" name="Status">
-                <option value="In-Progress">In-Progress</option>
-                <option value="Complete">Complete</option>
-            </select>
-  
-            <label for="taskAssignee">Choose Team Member:</label>
-              <select id="taskAssignee" name="taskAssignee">
-                  <option value="Member 1">Member 1</option>
-                  <option value="Member 2">Member 2</option>
-                  <option value="Member 3">Member 3</option>
-              </select><br><br>
-          </div>
-  
-        </div>
-      </div>  -->
+        <h3>Create a Task</h3>
+            <form id="createTaskForm">
+                <label for="taskTitle">Task Title:</label>
+                <input type="text" id="taskTitle" name="taskTitle" class="input-field" required><br><br>
 
-    <!-- <script src="scriptPKMS.js"></script> -->
+                <label for="taskDescription">Task Description:</label>
+                <textarea id="taskDescription" name="taskDescription" class="input-field" row = "4" cols ="50" style = "height: 105px; width: 505px" ></textarea><br><br>
+
+                <label for="taskDeadline">Task Deadline (YYYY-MM-DD):</label>
+                <input type="text" id="taskDeadline" name="taskDeadline" class="input-field" required><br><br>
+
+                <label for="assignedTo">Choose User:</label>
+                <select id="assignedTo" name="assignedTo" class="input-field" required>
+                </select><br><br>
+
+                <input id = "Project_ID" name = "Project_ID" type="hidden"> 
+
+                <button id = "SubmitB" type = "submit" class="submit-button">Create</button>
+            </form>
+    </section>
+    <!-- End of create a Task Modal -->
+
+    <!-- modal for Tasks -->
+    <section id = "taskModal" class="modal hidden"> 
+        <div class="flex">
+            <button class="btn-close" onclick = "closeModal()";>⨉</button>
+        </div>
+        <input id = "Task_ID" name = "Task_ID" type="hidden">
+        <input id = "Permission" name = "Permission" type="hidden"> 
+        <div id = "taskInfo"></div>
+    </section>
+    <!-- End of Task Modal -->
+    
+    <script> 
+        //pass the infomation about the countries into the java script
+        let User_ = "<?php echo  $_POST['User']?>";
+    </script>
+    <script src="ProdFunctions.js"></script>
 <body>
 </html>
