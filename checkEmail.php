ffa5c0b7a89ef721b52040d1d56b38fc9bdfc2b0
D0na1 <dsaji2004@gmail.com>
2024-02-19 01:36:15 +0000

    Invites

---
+++ checkEmail.php
@@ -0,0 +1,45 @@
+<?php
+
+error_reporting(E_ALL);
+ini_set("display_errors", 1);
+
+
+$email = $_GET['email'];
+
+$host = "localhost";
+$username = "team011";
+$password = "JAEWyfUXpzqank7scpWm";
+$dbname = "team011";
+
+$conn = mysqli_connect($host, $username, $password, $dbname);
+
+if (!$conn) {
+    echo json_encode(['status' => 'error', 'message' => 'Connection failed: ' . mysqli_connect_error()]);
+    exit;
+}
+
+$sql = "SELECT username,password FROM Users_Details WHERE user_email= ? AND username IS NULL AND password IS NULL;";
+
+
+
+
+$stmt = $conn->prepare($sql);
+
+if (!$stmt) {
+    echo json_encode(['status' => 'error', 'message' => "Error preparing statement: " . $conn->error]);
+    exit;
+}
+
+$stmt->bind_param("s",$email);
+
+if ($stmt->execute()) {
+
+    echo json_encode(['status' => 'success', 'emails' => $stmt->get_result()->fetch_row() ]);
+} else {
+    echo json_encode(['status' => 'failed', 'message' => "Error executing statement: " . $stmt->error]);
+}
+
+$stmt->close();
+$conn->close();
+
+?>
