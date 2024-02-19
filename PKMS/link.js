4b0557941dc636bc1b56655a002930f23ab8fce8
EmmanuelAdio <emmanueladio065@gmail.com>
2024-02-19 05:44:15 +0000

    Change to the hyper links.

--- PKMS/link.js
+++ PKMS/link.js
@@ -1,5 +1,5 @@
  $('#logoutB').click(function (){
-    window.location.href = "/PKMS/PKMS_Complete/Onyedikachi's%20code/signin.php";
+    window.location.href = "/signin.php";
 });
 
 function displayManager(role){
@@ -13,23 +13,23 @@ displayManager(role);
 
 //manger page essensials
 $('#ManageButton').click(function (){
-    window.location.href = "/PKMS/PKMS(Manager)/ManagerLanding.php";
+    window.location.href = "/PKMS(Manager)/ManagerLanding.php";
 });
 
 //manger page essensials
 $('#ProdButton').click(function (){
-    window.location.href = "/PKMS/PKMS(Productivity)/ProductivityLanding.php";
+    window.location.href = "/PKMS(Productivity)/ProductivityLanding.php";
 });
 
 //manger page essensials
 $('#KnowledgeButton').click(function (){
-    window.location.href = "/standard_index.php";
+    window.location.href = "/PKMS(Knowledge)/standard_index.php";
 });
 
 $('#RemindersButton').click(function (){
-    window.location.href = "/remindersLanding.php";
+    window.location.href = "/PKMS(Knowledge)/remindersLanding.php";
 });
 
 $('#InviteButton').click(function (){
-    window.location.href = "/invite.php";
+    window.location.href = "/PKMS(Knowledge)/invite.php";
 });
